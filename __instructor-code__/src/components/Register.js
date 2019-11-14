f1a8037ed23096a7f319d6880e03986fdfcc0442
WillHoll <willhollingsworth15@yahoo.com>
2019-11-14 12:32:46 -0700

    lunch time

--- __instructor-code__/src/components/Register.js
+++ __instructor-code__/src/components/Register.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Register extends Component {
   state = {
@@ -8,10 +9,43 @@ export default class Register extends Component {
     password2: ''
   }
 
+  handleChange = (key, value) => {
+    this.setState({
+      [key]: value
+    })
+  }
+
   render() {
     return (
       <div>
-        Register.js
+        <input
+          value={this.state.email}
+          onChange={e => this.handleChange('email', e.target.value)}
+          placeholder="Email"
+          type="text"
+        />
+        <input
+          value={this.state.name}
+          onChange={e => this.handleChange('name', e.target.value)}
+          placeholder="Name"
+          type="text"
+        />
+        <input
+          value={this.state.password1}
+          onChange={e => this.handleChange('password1', e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <input
+          value={this.state.password2}
+          onChange={e => this.handleChange('password2', e.target.value)}
+          placeholder="Retype password"
+          type="password"
+        />
+        <button>Register</button>
+        <Link to="/">
+          <h4>Already have an account? Login here</h4>
+        </Link>
       </div>
     )
   }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
   state = {
@@ -7,10 +8,24 @@ class Register extends Component {
     password1: '',
     password2: ''
   }
+
+  handleChange = (key, value) => {
+    this.setState({
+      [key]: value
+    })
+  }
+
   render() {
     return (
       <div>
-        Register.jsx
+        <input value={this.state.email} onChange={e => this.handleChange('email', e.target.value)} placeholder='Email' type="text" />
+        <input value={this.state.name} onChange={e => this.handleChange('name', e.target.value)} placeholder='Name ' type="text" />
+        <input value={this.state.password1} onChange={e => this.handleChange('password1', e.target.value)} placeholder='Password' type="password" />
+        <input value={this.state.password2} onChange={e => this.handleChange('password2', e.target.value)} placeholder='Retype password' type="password" />
+        <button>Register</button>
+        <Link to='/login'>
+          <h4>Already have an account? Login here</h4>
+        </Link>
       </div>
     );
   }
